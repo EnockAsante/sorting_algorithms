652c3e2f452a4d6384178f298dc929747a1e5389
Enock <asanteenoch10@gmail.com>
2024-04-07 18:20:47 +0200

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,15 +10,15 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] < array[j])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
